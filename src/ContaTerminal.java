999cfa49551aea6e194278b8c683320f676c927a
Joaoacl <joao_edu_ribeiro@outlook.com>
2025-04-24 17:41:37 -0300

    Adiciona todos os atributos a serem digitados e imprimi

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,8 +7,18 @@ public class ContaTerminal {
         System.out.println("Por favor, digite o número da Agência: ");
         String agencia = scanner.nextLine();
 
+        System.out.println("Por favor, digite o número da Conta: ");
+        int numero = scanner.nextInt();
+        scanner.nextLine();
 
-        System.out.println("Sua agência é: " + agencia);
+        System.out.println("Por favor, digite o nome do Cliente: ");
+        String nomeCliente = scanner.nextLine();
+
+        System.out.println("Por favor, digite o saldo: ");
+        double saldo = scanner.nextDouble();
+
+
+        System.out.println("Olá " + nomeCliente + " obrigado por criar uma conta em nosso banco, sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
 
         scanner.close();
     }
